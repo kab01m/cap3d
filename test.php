6fe33473f6789c52fcc14eda4b19714e2a63322b
kab01m <kab00m@lich.phys.spbu.ru>
2019-03-10 04:13:48 +0300

    firt

--- test.php
+++ test.php
@@ -2,4 +2,29 @@
 
 require '../php-gpio/vendor/autoload.php';
 
+use PhpGpio\Gpio;
+
+const ENABLE 24;
+const DIRX 23;
+const STX 17;
+
+
+$gpio = new GPIO();
+
+$gpio->setup (STX, "out");
+$gpio->setup (DIRX, "out");
+$gpio->setup (ENABLE, "out");
+
+$gpio->output(ENABLE, 1);
+$gpio->output(DIRX, 1);
+
+for ($i = 0; $i = 10; $i++) {
+	$gpio->output(STX, 1);
+	usleep (100);
+	$gpio->output(STX, 0);
+	usleep (500);
+}
+
+$gpio->output(ENABLE, 0);
+
 ?>
